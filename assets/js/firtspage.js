bfcce9cfdf784bc30a0f150d536088aa51cb76d5
Guilfornari <guilfornari@gmail.com>
2023-01-26 13:47:53 -0300

    Implemented function to check if basic info is accurate, check to see if it is valis URL is missing

--- assets/js/firtspage.js
+++ assets/js/firtspage.js
@@ -1,14 +1,14 @@
 let quizObjeto = {
-	title: "",
-	image: "",
-	questions:[],
-	levels:[]
+    title: "",
+    image: "",
+    questions: [],
+    levels: []
 }
 
 let i;
 let quizzesArray = []
 let firstPage = document.querySelector('.first-page')
-let quizCreation = document.querySelector('.quiz-creation')
+let quizCreation = document.querySelector('.quizz-creation')
 
 function pageCreateQuiz() {
     firstPage.classList.add('hidden');
@@ -16,16 +16,16 @@ function pageCreateQuiz() {
 }
 
 //função para obter os quizzes da api
-function getQuizzes(){
+function getQuizzes() {
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     promise.then(getQuizzesSucess)
 
 }
 //função para renderizar os quizzes e criar uma array com todos os objetos dos quizzes
 //elemento.data[i] === quizzesArray[i]
-function getQuizzesSucess(elemento){
+function getQuizzesSucess(elemento) {
     const showQuiz = document.querySelector('.all-quizzes')
-    for(i = 49; i >= 0; i = i-1){ 
+    for (i = 49; i >= 0; i = i - 1) {
         quizzesArray.unshift(elemento.data[i])
         showQuiz.innerHTML = `
         <div class="quizz" onclick="goToQuizz(this)">
