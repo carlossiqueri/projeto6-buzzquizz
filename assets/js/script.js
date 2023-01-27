64a470d772f6d48dd01277f7e752030c6b86a1fb
Evelyn <evelynbandeiramesquita@gmail.com>
2023-01-26 21:10:45 -0300

    Reduzindo css

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function goToFirstPage() {
-
-    if (firstPage = document.querySelector('.first-page.hidden')) {
-        firstPage.classList.remove('hidden')
-        quizCreation.classList.add('hidden')
-    }
-
-    
-}
