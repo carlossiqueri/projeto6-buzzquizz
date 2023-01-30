0f2888dfb467eec9a6f71cdcef305bddf24b0957
Carlos <carlsdias.dev@gmail.com>
2023-01-30 12:45:20 -0300

    responsive tela 2 - quizz page

--- assets/js/quiz-page.js
+++ assets/js/quiz-page.js
@@ -1,3 +1,7 @@
+let arrayRespostas = [];
+let retorna;
+
+
 function refresh() {
     window.location.reload()
 }
@@ -8,8 +12,56 @@ function compare() {
 //FUNÇÃO QUE LIMPA A HOME E RENDERIZA TODO O QUIZ SELECIONADO
 
 function goToQuiz(quizz) {
-    //teste buscando id do quiz selecionado
-    const elementIdQuiz = quizz.getAttribute('id');
+    renderQuizzPage();    
+    renderQuizz();
+}
+
+function renderQuizzPage(){
+    document.querySelector('.first-page').classList.add('hidden');
+    let quizzPage = document.querySelector('.quizzPage');
+    quizzPage.classList.remove('hidden');
+    quizzPage.innerHTML = "";
     
-    return console.log(elementIdQuiz)
+    
+}
+
+function renderQuizz() {
+    let banner = document.querySelector('.quizzBanner');
+    let questoes = document.querySelector('.quizzAnswers');
+    questoes.innerHTML
+   
+
+    banner.innerHTML = 
+    `
+    <div class="quizzBanner">   
+                <img src="${quizz.image}" alt="">
+                <h3>${quizz.title}</h3>
+            </div>
+    `
+
+    for (let i = 0; i < retorna.questoes.length; i++){
+        questoes.innerHTML = 
+        `
+        <div class="quizzAnswers">
+        <div>
+            <h4>Qual é a comida mais famosa?</h4>
+        </div>
+        <div class="containerQuizzes">
+            <div>
+                <img src="./assets/img/brownie.jpeg" alt="" class="choosenAnswer">
+                <p>bxcxbbxcv</p>
+            </div>
+            <div>
+                <img src="./assets/img/carbonara.jpeg" alt="" class="choosenAnswer">
+                <p>zxczc</p>
+            </div>
+            <div>
+                <img src="./assets/img/bife-a-milanesa.jpeg" alt="" class="choosenAnswer">
+                <p>bife a asdasdsada</p>
+            </div>
+            <div>
+                <img src="./assets/img/a-la-minuta.jpeg" alt="">
+                <p>asdsdsad</p>
+        `
+    }
 }
